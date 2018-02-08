6ad56d94e5977f4ac243342d337bb1b13fcfb518
Hernawan Fa'iz Abdillah <account@abdillah.me>
2018-02-08 07:52:49 +0700

    Fix compilation files not included in conf object

--- index.js
+++ index.js
@@ -89,7 +89,7 @@ Plugin.prototype.apply = function (compiler) {
             }
           });
         });
-        merge(obj.source, { include: files });
+        obj.source = merge(obj.source, { include: files });
       }
 
       /**
